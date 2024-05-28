12f97250a8dccc0b68ac1a42aabcd4298d1b6a58
netw3173 <113054391+netw3173@users.noreply.github.com>
2024-05-28 10:48:17 -0400

    Add files via upload

---
+++ precache-manifest.fb71545679dd5af3f34c966e4707697a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f747e525e4f855f7c57a",
+    "url": "/static/css/main.fb63ca57.chunk.css"
+  },
+  {
+    "revision": "f747e525e4f855f7c57a",
+    "url": "/static/js/main.f747e525.chunk.js"
+  },
+  {
+    "revision": "cc67335e6918054c8f48",
+    "url": "/static/js/1.cc67335e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "032e2d186564b6c1b5a297bec4109b9d",
+    "url": "/index.html"
+  }
+];
